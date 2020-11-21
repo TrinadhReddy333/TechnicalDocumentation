daec8268bc6e00b57f50490cb7ff957211a28bcb
TrinadhReddy333 <73431882+TrinadhReddy333@users.noreply.github.com>
2020-11-21 17:43:17 +0530

    Changed my name

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Insights for Telecom  Call Data Analytics'
+copyright = '2020, TrinadhReddy'
+author = 'TrinadhReddy'
 
 # The full version, including alpha/beta/rc tags
 release = ''
